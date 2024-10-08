4aabc45d94dd93c38bda08e9150f9d1a94172a7d
Anurag <coleridgeprotube@gmail.com>
2024-10-09 01:28:25 +0530

    improve search

--- src/main.rs
+++ src/main.rs
@@ -2,9 +2,7 @@ use minigrep::Config;
 use std::env;
 use std::process;
 fn main() {
-    let args: Vec<String> = env::args().collect();
-
-    let config = Config::build(&args).unwrap_or_else(|err| {
+    let config = Config::build(env::args()).unwrap_or_else(|err| {
         eprintln!("Error passing arguments: {err}");
         process::exit(1)
     });
